3a875c75e3759f5953ca546f0d0c51402ff0d7e8
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 00:42:11 -0500

    states and population and capital

---
+++ states.js
@@ -0,0 +1,13 @@
+var STATES = [
+  	{name: "Indiana", population: 6597000, capital: "Indianapolis"},
+	{name: "Ohio", population: 11430000, capital: "Columbus"},
+	{name: "Illinois", population: 12880000, capital: "Springfield"},
+	{name: "Michigan", population: 9910000, capital: "Lansing"},
+	{name: "Minnesota", population: 5457000, capital: "Saint Paul"},
+	{name: "Wisconsin", population: 5758000, capital: "Madison"},
+	{name: "Kentucky", population: 4413000, capital: "Frankfort"},
+	      ];
+
+if (typeof module != "undefined" && module.exports)
+	module.exports = MOUNTAINS;
+
